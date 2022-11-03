3d63fc307df848fc67c462728a55830c0bf66a00
Fujino <fujitomo375@gmail.com>
2022-11-03 09:19:55 +0900

    ベース用を保存

---
+++ crawl/keepa_test.py
@@ -0,0 +1,51 @@
+import sys
+import os
+sys.path.append(os.path.abspath(".."))
+
+from crawl.keepa import *
+
+
+def test_fetch_products():
+    keepa = KeepaAPI()
+    response = keepa.fetch_products(
+        asins=[
+            "B07ZCSDPD3",
+            "B01BEPNPF6",
+            "B07RWWZT8J"
+        ]
+    )
+    print(response)
+    
+    assert len(response) == 3
+    assert response[0].title 
+    assert response[0].price 
+    
+
+def test_fetch_products_by_jan():
+    keepa = KeepaAPI()
+    response = keepa.fetch_products(
+        jan_codes= [
+            "4953759010027",
+            "4901987254256",
+            "4580543940866"
+        ]
+    )
+    print(response)
+    
+    assert len(response) == 3
+    assert response[0].title 
+    assert response[0].price 
+    
+    
+def test_fetch_best_seller():
+    keepa = KeepaAPI()
+    response = keepa.fetch_best_seller_product_asins("71588051")
+    print(response)
+    
+    assert response
+    
+    
+def test_token_status():
+    keepa = KeepaAPI()
+    res = keepa.get_tokens_left()
+    print(res)
---
+++ main/run.py
@@ -0,0 +1,116 @@
+# importの順番は慣例的には以下の順
+# pip install 不要なもの -> pip installが必要なもの -> 自作ライブラリ
+import math
+import sys
+from datetime import datetime
+from pathlib import Path
+from tkinter import E
+from typing import List, Optional
+
+import fire
+import numpy as np
+import pandas as pd
+
+# ProjectRootをライブラリimport要のPashに追加することで、以下の自作ライブラリimport可能になる
+ROOT_PATH = str(Path(__file__).absolute().parent.parent)
+sys.path.append(ROOT_PATH)
+from common.logger import set_logger
+from crawl.keepa import KeepaAPI
+
+logger = set_logger(__name__)
+
+now = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+ASIN_LIST_CSV_PATH = f"{ROOT_PATH}/files/asin_list.csv"
+JAN_LIST_CSV_PATH = f"{ROOT_PATH}/files/jan_list.csv"
+FETCHED_PRODUCTS_CSV_PATH = f"{ROOT_PATH}/files/fetched_products_{now}.csv"
+
+
+def fetch_ranking_products_to_csv(category_id: str):
+    logger.info("[start]")
+    keepa = KeepaAPI()
+    asins = keepa.fetch_best_seller_product_asins(category_id)
+    print(asins)
+    logger.info(f"asins_count={len(asins)}")
+    fetch_product_to_csv(asins)
+    logger.info("[completed]")
+
+
+def fetch_products(mode: str, search_keys: list):
+    # KeepaAPIクラスを使用して商品情報を取得
+    keepa = KeepaAPI()
+    
+    # KeepaAPIは１回に最大１００件までしか取得できないため、100件以上の場合は、100件づつのリストに分割する
+    n = math.ceil(len(search_keys) / 100) # 分割数
+    splited_search_keys_list = np.array_split(search_keys, n) # 100個毎のリスト × n個 に分割
+    logger.info(f"split_number={n}")
+    
+    # n回APIをコールして、結果をまとめる
+    products = []
+    for splited_search_keys in splited_search_keys_list:
+        if mode == "asin":
+            products.extend(keepa.fetch_products(asins=list(splited_search_keys)))
+        else:
+            products.extend(keepa.fetch_products(jan_codes=list(splited_search_keys)))
+    logger.info(f"fetched items count={len(products)}")
+    
+    return products
+    
+
+def export_csv(products: pd.DataFrame):
+    
+    products.to_csv(
+        FETCHED_PRODUCTS_CSV_PATH, 
+        mode="w", 
+        encoding="utf-8_sig",
+        columns=[
+            "asin",
+            "jan",
+            "title",
+            "price",
+            "description",
+            "url",
+            "thumbnail_urls"
+        ],
+        header=[
+            "ASIN",
+            "JAN",
+            "タイトル",
+            "価格",
+            "説明",
+            "URL",
+            "サムネイルURLs"
+        ]
+    )           
+    
+
+def fetch_products_by_csv(mode: str , limit: int=None):
+    try:
+        # 取得するASIN情報の読み込み
+        if mode == "asin":
+            search_keys_list = pd.read_csv(ASIN_LIST_CSV_PATH, dtype="str")
+        elif mode == "jan":
+            search_keys_list = pd.read_csv(JAN_LIST_CSV_PATH, dtype="str")
+        else:
+            raise Exception("modeは、asin か jan を指定してください")
+        in_search_keys = list(search_keys_list[mode])[:limit] if limit and limit >= 1 else list(search_keys_list[mode])
+        logger.info(f"[start] asin_list={in_search_keys}")
+        products = fetch_products(mode, in_search_keys)
+        products_dict = []
+        for product in products:
+            products_dict.append(product.to_dict())
+        products_df = pd.DataFrame.from_dict(products_dict)
+        result_df = pd.merge(search_keys_list, products_df, how="outer")
+        print(result_df)
+        export_csv(result_df)
+        
+        logger.info("[completed]")
+    except Exception as e:
+        logger.error(f"[failed] message={e}")
+        # 通常のエラーでは、詳細な原因が分からない場合があるので、tracebackを使用して詳細なエラーを出力する
+        import traceback
+        logger.error(traceback.format_exc())
+    
+    
+if __name__ == "__main__":
+    fire.Fire()
